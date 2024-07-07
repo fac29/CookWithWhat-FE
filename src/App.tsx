af04b67fc52d0a3d33d254dc4b2f48be68c7fecd
thatGui <thatguifonseca@gmail.com>
2024-07-07 13:51:20 +0100

    adding tailwind css globals

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import Button from "./components/Button/Button";
 //import './App.css';
 
@@ -9,7 +9,7 @@ export default function App() {
                 <Button
                     btnText="hello"
                     btnonClick={() => console.log("hello")}
-                    btnclassName="btnPrimary"
+                    btnclassName="color-red bg-cprimary py-[8px] px-[16px] border-2 border-black rounded-[8px]"
                 />
             </div>
         </>
