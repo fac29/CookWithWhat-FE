8c44a4e44d77d3fbc054e72040f18be47e801fc3
thatGui <thatguifonseca@gmail.com>
2024-07-07 13:33:58 +0100

    cssReset

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { btnProps } from "../../Utils/TypeBucket";
+
+export default function Button({
+    btnText,
+    btnonClick,
+    btnclassName,
+}: btnProps) {
+    return (
+        <button className={btnclassName} onClick={btnonClick} type="submit">
+            {btnText}
+        </button>
+    );
+}
